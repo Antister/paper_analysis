ae7a0b04bb90b4e45ba6ce349f53193393909c51
Mirika <218917423+Antister@users.noreply.github.com>
2025-07-03 18:35:56 +0800

    Add MD5 vaildation feature

--- src/fetch.py
+++ src/fetch.py
@@ -1,49 +1,73 @@
 import os
 import gzip
 import logging
+import hashlib
 import urllib.request
 
 
 class DataFetcher:
     FILES = {
         "dblp.xml.gz": "https://dblp.org/xml/dblp.xml.gz",
-        "dblp.dtd": "https://dblp.org/xml/dblp.dtd",
+        "dblp.xml.gz.md5": "https://dblp.org/xml/dblp.xml.gz.md5",
     }
-    CACHE_DIR_NAME = "cache"
 
-    def __init__(self) -> None:
+    def __init__(self, cache_dir: str) -> None:
+        self.__cache_dir = cache_dir
+
         self.__logger = logging.getLogger("[DataFetcher]")
 
     def fetch(self) -> None:
+        files = list(self.FILES.keys())
+
         try:
             self.__do_net_fetch(self.FILES)
-            self.__do_unzip(list(self.FILES.keys())[0])
+
+            if not self.__do_integrity_check(files[0], files[1]):
+                self.__logger.error(
+                    f'MD5 checksum mismatch between "{files[0]}" and "{files[1]}"'
+                )
+                raise RuntimeError(f"Corrupted file: {files[0]}")
+
+            self.__do_unzip(files[0])
         except Exception as e:
-            self.__logger.error(f'Fetch task failed with "{e}"')
+            err = f'Fetch task failed with "{e}"'
+            self.__logger.error(err)
+            raise RuntimeError(err)
 
     def __do_net_fetch(self, url_file_map: dict[str, str]) -> None:
         self.__logger.info(f"Try to download all files: {list(url_file_map.keys())}")
 
         for filename, url in url_file_map.items():
-            full_filename = os.path.join(self.CACHE_DIR_NAME, filename)
+            full_filename = os.path.join(self.__cache_dir, filename)
             if not os.path.exists(full_filename):
                 self.__logger.info(f"Fetching: {full_filename}, {url}")
 
                 with urllib.request.urlopen(url=url, timeout=90.0) as response:
-                    os.makedirs(self.CACHE_DIR_NAME, 0o755, True)
+                    os.makedirs(self.__cache_dir, 0o755, True)
                     with open(full_filename, "wb+") as file:
                         file.write(response.read())
+
+    def __do_integrity_check(self, filename: str, md5_filename: str) -> bool:
+        self.__logger.info(f"Validating download file: {filename}")
+
+        hasher = hashlib.md5()
+        with open(os.path.join(self.__cache_dir, filename), "rb") as file:
+            while data := file.read(2**20):
+                hasher.update(data)
+
+        with open(os.path.join(self.__cache_dir, md5_filename), "r") as file:
+            checksum = file.read()
+            checksum = checksum.split()[0]
+            return checksum == hasher.hexdigest()
 
     def __do_unzip(self, zip_filename: str) -> None:
         final_filename, ext = os.path.splitext(zip_filename)
         if ext not in [".gz", ".tgz"]:
             raise RuntimeError(f"Compression format: {ext} is not supported")
 
-        if not os.path.exists(os.path.join(self.CACHE_DIR_NAME, final_filename)):
-            self.__logger.info("Unzipping files")
+        if not os.path.exists(os.path.join(self.__cache_dir, final_filename)):
+            self.__logger.info(f"Unzipping file: {zip_filename}")
 
-            with gzip.open(os.path.join(self.CACHE_DIR_NAME, zip_filename)) as source:
-                with open(
-                    os.path.join(self.CACHE_DIR_NAME, final_filename), "wb"
-                ) as dest:
+            with gzip.open(os.path.join(self.__cache_dir, zip_filename)) as source:
+                with open(os.path.join(self.__cache_dir, final_filename), "wb") as dest:
                     dest.write(source.read())
